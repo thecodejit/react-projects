ad6e4dd940f0ce05199523bf804071554c352efe
thecodejit <43138013+thecodejit@users.noreply.github.com>
2023-07-17 22:54:20 +0530

    Initial Commit
    App using concepts such as useEffect, Cleanup function, Ternary operators and Short circuit evaluation.

---
+++ tourapp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      useEffect(()=>{
+
+      },[])
+    </>
+  )
+}
+
+export default App
